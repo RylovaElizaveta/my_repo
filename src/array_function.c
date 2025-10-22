167d15bf4ac14d3831e49bb159ca6a59528f29db
Рылова Елизавета Максимовна ИП-517 <lizarylova55@gmail.com>
2025-10-22 18:46:51 +0700

    ЛР6

--- src/array_function.c
+++ src/array_function.c
@@ -37,5 +37,18 @@ void join_and_sort_int_arrays(int* src1, size_t size_src1, int* src2, size_t siz
     }
 }
 
-void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {}
+void get_min_and_max_from_int_array(int* src, size_t size, int* min, int* max) {
+    *min = src[0];
+    *max = src[0];
+
+    for (size_t i = 1; i < size; i++) {
+        if (src[i] < *min) {
+            *min = src[i];
+        }
+
+        if (src[i] > *max) {
+            *max = src[i];
+        }
+    }
+}
 
